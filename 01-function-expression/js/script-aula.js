8e4843637eba103abfb4a8165f3471110bc85f60
Gabriel Marques <bielmarques922@gmail.com>
2022-03-09 15:53:45 -0300

    Function Expression - Parte 2

--- 01-function-expression/js/script-aula.js
+++ 01-function-expression/js/script-aula.js
@@ -1,11 +1,27 @@
 // EXERCÍCIOS
 // Remova o erro
+
+/* Erro
 priceNumber('R$ 99,99');
 const priceNumber = n => +n.replace('R$', '').replace(',', '.');
+*/
 
-// Crie uma IIFE e isole o escopo
-// de qualquer código JS.
+const priceNumber = function (n){
+  return +n.replace('R$', '').replace(',', '.');
+} 
+console.log(priceNumber('R$ 99,99'));
 
-// Como podemos utilizar
-// a função abaixo.
+
+// Crie uma IIFE e isole o escopo de qualquer código JS.
+(function(){
+  const nome = 'Gabriel';
+  console.log(nome);
+})();
+
+
+// Como podemos utilizar a função abaixo.
 const active = callback => callback();
+
+active(function() {
+  console.log('Teste de Active');
+})
